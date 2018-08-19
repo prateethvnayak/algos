e9f5e6aa5f809001a7d4b07b0fa0c451140e26ba
Prateeth Nayak <nayak044@umn.edu>
2018-08-18 19:52:16 -0700

    week 3 update

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -1,10 +1,31 @@
 # Uses python3
 import sys
+from time import time
+
 
 def get_change(m):
-    #write your code here
-    return m
+    # write your code here
+    i = 0
+    coin = [0, 0, 0]
+    V = [10, 5, 1]
+    # print(m, '\n')
+    while m > 0:
+        if min(m, V[i]) < m:
+            m = m - V[i]
+            coin[i] += 1
+        elif V[i] == m:
+            m = m - V[i]
+            coin[i] += 1
+            break
+        else:
+            i += 1
+
+    return sum(coin)
+
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
+    # s = time()
     print(get_change(m))
+    # e = time()
+    # print('\n ',(e - s))
--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -1,9 +1,21 @@
 # Uses python3
 import sys
 
+
 def get_optimal_value(capacity, weights, values):
     value = 0.
     # write your code here
+    val_unit = [float(val) / float(wght) for val, wght in zip(values, weights)]
+    for i in range(len(weights)):
+        if capacity == 0:
+            return value
+        valunit = max(val_unit)
+        i = val_unit.index(valunit)
+        val_unit[i] = -1
+        a = min(weights[i], capacity)
+        value += (a * valunit)
+        weights[i] -= a
+        capacity -= a
 
     return value
 
@@ -14,4 +26,4 @@ if __name__ == "__main__":
     values = data[2:(2 * n + 2):2]
     weights = data[3:(2 * n + 2):2]
     opt_value = get_optimal_value(capacity, weights, values)
-    print("{:.10f}".format(opt_value))
+    print("{:.4f}".format(opt_value))
